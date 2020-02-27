d342af74da1a80eac70446a347c54a9c10f2676e
Francisco de Souza Junior <fsjunior@gmail.com>
2020-02-27 15:17:33 -0300

    Added deleted tweets counter.

--- twitter_cleaner/app.py
+++ twitter_cleaner/app.py
@@ -5,9 +5,7 @@ import time
 from tqdm import tqdm
 
 
-
-
-class App:
+class App(object):
     def __init__(self, args):
         auth = tweepy.OAuthHandler(args.consumer_key, args.consumer_secret)
         auth.set_access_token(args.access_token_key, args.access_token_secret)
@@ -22,25 +20,28 @@ class App:
 
         print("Filtering tweets...")
 
-        list_of_ids = self.ar.filter_by_timestamp(timestamp_filter)
+        list_of_ids = self.ar.filter(timestamp_filter)
 
         print("Tweets that matches the date filter: {}".format(len(list_of_ids)))
 
         print("Deleting tweets...")
-        for id in tqdm(list_of_ids):
+
+        deleted_tweets_count = 0
+        for tweet_id in tqdm(list_of_ids):
             try:
-                self.api.destroy_status(id=id)
+                self.api.destroy_status(id=tweet_id)
+                deleted_tweets_count += 1
             except tweepy.error.TweepError as e:
                 if e.api_code == 144:
-                    print("Tweet with id {} already deleted, skipping.".format(id))
+                    print("Tweet with id {} already deleted, skipping.".format(tweet_id))
                 elif e.api_code in [88, 185]:
                     print("Rate limit reached. Waiting for 5 minutes until trying again...")
                     for _ in tqdm(range(60)):
                         time.sleep(1)
                 else:
-                    print("Unknown error. Exiting.")
                     print(e)
+                    print("Unknown error. Exiting.")
+                    return 1
+        print("Finished! {} tweets deleted.".format(deleted_tweets_count))
 
-
-
-
+        return 0
--- twitter_cleaner/archivefilter.py
+++ twitter_cleaner/archivefilter.py
@@ -2,13 +2,13 @@ import pandas as pd
 from datetime import datetime
 
 
-class ArchiveFilter:
+class ArchiveFilter(object):
 	def __init__(self, file):
 		self.tweet_data = pd.read_csv(file)
 
-		# convert str to timestamp
+		# convert str to timestamp and set timezone to None to get naive timestamp
 		self.tweet_data.timestamp = pd.to_datetime(self.tweet_data.timestamp).dt.tz_localize(None)
 
-	def filter_by_timestamp(self, older_than):
+	def filter(self, older_than):
 		return self.tweet_data[self.tweet_data.timestamp < older_than].tweet_id
 
